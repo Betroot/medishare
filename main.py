2edc1c1ad57333d709d7d5525bc389784eb7696d
Johannes <S3425242@student.rmit.edu.au>
2023-05-20 14:05:51 +0800

    commit

---
+++ main.py
@@ -0,0 +1,147 @@
+from flask import Flask, request, redirect, render_template, session, url_for, flash, jsonify, logging
+import logging
+import boto3
+
+app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+handler = logging.FileHandler('/var/log/apache2/myapp.log')
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
+s3 = boto3.client('s3')
+bucket_name = "music-bucket340822"
+
+import utils
+
+
+@app.route('/')
+def root():
+    utils.create_music_table()
+    utils.load_music()
+    utils.create_login_table()
+    utils.load_login_data()
+    utils.load_image_url()
+    return render_template(
+        'login.html')
+
+
+@app.route('/login', methods=['GET', "POST"])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    email = request.form['email']
+    password = request.form['password']
+
+    user = utils.validate_user(email, password)
+    if user:
+        session['email'] = email
+        session['user_name'] = user['user_name']
+        resp = redirect(url_for('forum'))
+        return resp
+    else:
+        error_msg = 'email or password is invalid'
+        return render_template('login.html', error_msg=error_msg)
+
+
+@app.route('/register', methods=['GET', "POST"])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        user_name = request.form['user_name']
+        password = request.form['password']
+
+        if utils.is_email_exist(email):
+            error_message = 'The email already exists'
+            return render_template('register.html', error_msg=error_message)
+
+        utils.insert_user(email, user_name, password)
+        return redirect(url_for('login'))
+
+
+@app.route('/forum', methods=['GET', "POST"])
+def forum():
+    user_name = session.get("user_name")
+    return render_template("forum.html", user_name=user_name)
+    return redirect(url_for("login"))
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout_():
+    del session['email']
+    del session['user_name']
+    return redirect('/login')
+
+
+@app.route('/perform-query', methods=['GET'])
+def perform_query():
+    title = request.args.get('title')
+    year = request.args.get('year')
+    artist = request.args.get('artist')
+
+    if title is None:
+        title = ""
+    if year is None:
+        year = ""
+    if artist is None:
+        artist = ""
+
+    response = utils.query_music(title, year, artist)
+    if response['Count'] == 0:
+        return jsonify({'message': 'No result is retrieved. Please query again.'}), 200
+    music_list = []
+    for item in response['Items']:
+        music_info = {
+            'title': item['title'],
+            'year': item['year'],
+            'artist': item['artist']
+        }
+        image_url = item['img_url']
+        image_name = image_url.split('/')[-1]
+        image_signed_url = s3.generate_presigned_url('get_object', Params={'Bucket': bucket_name, 'Key': image_name})
+        music_info['image_url'] = image_signed_url
+        music_list.append(music_info)
+    return jsonify( music_list)
+
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    title = request.get_json()['title']
+    year = request.get_json()['year']
+    artist = request.get_json()['artist']
+    email = session.get('email')
+    img_url = request.get_json()['img_url']
+    utils.create_subscribe_table()
+    utils.insert_subscribe(email,title,year,artist,img_url)
+    return jsonify({'success': True})
+
+@app.route('/remove_subscribe', methods=['POST'])
+def remove_subscribe():
+    title = request.get_json()['title']
+    year = request.get_json()['year']
+    artist = request.get_json()['artist']
+    email = session.get('email')
+    utils.delete_subscribe(email,title,year,artist)
+    return jsonify({'success': True})
+
+
+@app.route('/get_subscription', methods=['GET'])
+def get_subscription():
+    email = session.get('email')
+    response = utils.query_subscription_by_email(email)
+    music_list = []
+    for item in response['Items']:
+
+        music_info = {
+            'title': item['title'],
+            'year': item['year'],
+            'artist': item['artist'],
+            'img_url': item['img_url']
+        }
+        music_list.append(music_info)
+    return jsonify(music_list)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ utils.py
@@ -0,0 +1,289 @@
+import boto3
+from botocore.exceptions import ClientError
+import json
+import io
+from main import app
+import requests
+from boto3.dynamodb.conditions import Key, Attr
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+s3 = boto3.client('s3')
+bucket_name = "music-bucket340822"
+
+
+# load image url
+def load_image_url():
+    with open("/var/www/myapp/a2.json") as json_file:
+        data = json.load(json_file)
+
+    songs = data['songs']
+    for song in songs:
+        image_url = song['img_url']
+        filename = image_url.split('/')[-1]
+        response = requests.get(image_url)
+        if response.status_code != 200:
+            app.logger.info(f"Error downloading {image_url}: {response.status_code}")
+        try:
+            with io.BytesIO(response.content) as file_obj:
+                s3.upload_fileobj(file_obj, bucket_name, filename)
+                app.logger.info(f"Uploaded {filename} to S3 bucket.")
+        except Exception as e:
+            app.logger.info(f"Error uploading {filename} to S3 bucket: {e}")
+
+
+def query_music(title, year, artist):
+    table = dynamodb.Table('music')
+    response = table.scan(
+        FilterExpression=Attr('title').contains(title) & Attr('year').contains(year) & Attr('artist').contains(artist)
+    )
+    return response
+
+
+def validate_user(email, password):
+    login_table = dynamodb.Table('login')
+
+    try:
+        response = login_table.get_item(
+            Key={
+                'email': email
+            }
+        )
+    except ClientError as e:
+        app.logger.info(e.response['error']['message'])
+        return False
+
+    else:
+        # check whether response contains item and password matches
+        if 'Item' in response and response['Item']['password'] == password:
+            return response['Item']
+        else:
+            return False
+
+
+def is_email_exist(email):
+    table = dynamodb.Table('login')
+    response = table.get_item(Key={'email': email})
+    if 'Item' in response:
+        return True
+    return False
+
+
+def insert_subscribe(email, title, year, artist, img_url):
+    table = dynamodb.Table('subscribe')
+    table.put_item(Item={'email': email, 'title': title, 'year': year, 'artist': artist, 'img_url': img_url})
+
+
+def delete_subscribe(email, title, year, artist):
+    table = dynamodb.Table('subscribe')
+    try:
+        response = table.delete_item(
+            Key={
+                'email': email,
+                'title': title
+            },
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        return response
+
+
+def query_subscription_by_email(email):
+    table = dynamodb.Table('subscribe')
+    response = table.query(
+        KeyConditionExpression=Key('email').eq(email)
+    )
+    return response
+
+
+def insert_user(email, username, password):
+    table = dynamodb.Table('login')
+    table.put_item(Item={'email': email, 'user_name': username, 'password': password})
+
+
+def create_subscribe_table():
+    try:
+        table = dynamodb.Table('subscribe')
+        table.table_status
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # Table does not exist, so create it
+            table = dynamodb.create_table(
+                TableName='subscribe',
+                KeySchema=[
+                    {
+                        'AttributeName': 'email',
+                        'KeyType': 'HASH'
+                    },
+                    {
+                        'AttributeName': 'title',
+                        'KeyType': 'RANGE'
+                    }
+                ],
+                AttributeDefinitions=[
+                    {
+                        'AttributeName': 'email',
+                        'AttributeType': 'S'
+                    },
+                    {
+                        'AttributeName': 'title',
+                        'AttributeType': 'S'
+                    }
+                ],
+                ProvisionedThroughput={
+                    'ReadCapacityUnits': 5,
+                    'WriteCapacityUnits': 5
+                }
+            )
+            table.wait_until_exists()
+        else:
+            raise e
+    else:
+        print(f"Table subscribe already exists.")
+
+
+def create_login_table():
+    try:
+        table = dynamodb.Table('login')
+        table.table_status
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            table = dynamodb.create_table(
+                TableName='login',
+                KeySchema=[
+                    {
+                        'AttributeName': 'email',
+                        'KeyType': 'HASH'
+                    }
+                ],
+                AttributeDefinitions=[
+                    {
+                        'AttributeName': 'email',
+                        'AttributeType': 'S'
+                    }
+                ],
+                ProvisionedThroughput={
+                    'ReadCapacityUnits': 5,
+                    'WriteCapacityUnits': 5
+                }
+            )
+            table.wait_until_exists()
+        else:
+            raise e
+    else:
+        print(f"Table login already exists.")
+
+
+def load_login_data():
+    table = dynamodb.Table('login')
+    items = [
+        {
+            'email': 's34252420@student.rmit.edu.au',
+            'user_name': 'Bai Lin Johannes Ao0',
+            'password': '012345'
+        },
+        {
+            'email': 's34252421@student.rmit.edu.au',
+            'user_name': 'Bai Lin Johannes Ao1',
+            'password': '123456'
+        },
+        {
+            'email': 's34252422@student.rmit.edu.au',
+            'user_name': 'Bai Lin Johannes Ao2',
+            'password': '234567'
+        },
+        {
+            'email': 's34252423@student.rmit.edu.au',
+            'user_name': 'Bai Lin Johannes Ao3',
+            'password': '345678'
+        },
+        {
+            'email': 's34252424@student.rmit.edu.au',
+            'user_name': 'Bai Lin Johannes Ao4',
+            'password': '456789'
+        },
+        {
+            'email': 's34252425@student.rmit.edu.au',
+            'user_name': 'Bai Lin Johannes Ao5',
+            'password': '567890'
+        },
+        {
+            'email': 's34252426@student.rmit.edu.au',
+            'user_name': 'Bai Lin Johannes Ao6',
+            'password': '678901'
+        },
+        {
+            'email': 's34252427@student.rmit.edu.au',
+            'user_name': 'Bai Lin Johannes Ao7',
+            'password': '789012'
+        },
+        {
+            'email': 's34252428@student.rmit.edu.au',
+            'user_name': 'Bai Lin Johannes Ao8',
+            'password': '890123'
+        },
+        {
+            'email': 's34252429@student.rmit.edu.au',
+            'user_name': 'Bai Lin Johannes Ao9',
+            'password': '901234'
+        }
+    ]
+
+    with table.batch_writer() as batch:
+        for item in items:
+            batch.put_item(Item=item)
+
+
+def create_music_table():
+    table_name = 'music'
+    try:
+        table = dynamodb.Table(table_name)
+        table.table_status
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            table = dynamodb.create_table(
+                TableName=table_name,
+                KeySchema=[
+                    {
+                        'AttributeName': 'artist',
+                        'KeyType': 'HASH'
+                    },
+                    {
+                        'AttributeName': 'title',
+                        'KeyType': 'RANGE'
+                    }
+                ],
+                AttributeDefinitions=[
+                    {
+                        'AttributeName': 'artist',
+                        'AttributeType': 'S'
+                    },
+                    {
+                        'AttributeName': 'title',
+                        'AttributeType': 'S'
+                    }
+                ],
+                ProvisionedThroughput={
+                    'ReadCapacityUnits': 5,
+                    'WriteCapacityUnits': 5
+                }
+            )
+            table.wait_until_exists()
+        else:
+            raise e
+    else:
+        print(f"Table {table_name} already exists.")
+
+
+def load_music():
+    table_name = 'music'
+    table = dynamodb.Table(table_name)
+    with open("/var/www/myapp/a2.json") as json_file:
+        data = json.load(json_file)
+
+    songs = data['songs']
+    for song in songs:
+        table.put_item(Item=song)
+
+    app.logger.info(f"Data loaded into {table_name} table.")
